8f278cf79ffb72cb1bc0e16a9213f80e3e8e5b50
Emanuela Buganik <emanuela.buganik@student.etf.unibl.org>
2024-02-15 00:12:27 +0100

    Update project.h

--- CCES/project/src/project.h
+++ CCES/project/src/project.h
@@ -9,7 +9,7 @@
 
 void delay_effect(const float *audio_data, float *audio_output, float delay_s, float amplitude);
 void echo_effect(const float *audio_data, float *audio_output, int stages, float delay_s, float amplitude);
-float lfo_function(int i);
+float lfo(int i);
 void tremolo_effect(const float *audio_data, float *audio_output);
 void flanger_effect(const float *audio_data, float *audio_output, float delay_s);
 void bit_crusher_effect(const float *audio_data, float *audio_output, int D, int bit_depth);
